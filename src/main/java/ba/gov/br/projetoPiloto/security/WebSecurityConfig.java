1801ce32226739704972d8e637e77371a3893607
Thaylow <thaylowviana@gmail.com>
2019-12-11 15:27:14 -0300

    Spring Security

---
+++ src/main/java/ba/gov/br/projetoPiloto/security/WebSecurityConfig.java
@@ -0,0 +1,37 @@
+package ba.gov.br.projetoPiloto.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfig 
+extends WebSecurityConfigurerAdapter
+{
+
+	 @Override
+	    protected void configure(AuthenticationManagerBuilder auth)
+	      throws Exception {
+	        auth
+	          .inMemoryAuthentication()
+	          .withUser("user")
+	            .password("password")
+	            .roles("USER")
+	            .and()
+	          .withUser("ProjetoPiloto")
+	            .password("abc")
+	            .roles("USER", "ADMIN");
+	    }
+	 
+	    @Override
+	    protected void configure(HttpSecurity http) throws Exception {
+	        http
+	          .authorizeRequests()
+	          .anyRequest()
+	          .authenticated()
+	          .and()
+	          .httpBasic().and (). csrf (). disable();
+	    }
+	    
+}
